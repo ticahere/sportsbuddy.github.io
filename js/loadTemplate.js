b0dfb91d0bfe908f3b6b3b9094e6d973c9edb038
Tica Lin <ticalin@Ticas-MacBook-Pro-2.local>
2024-08-14 17:19:56 +0200

    loadTemplate

---
+++ js/loadTemplate.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const headPromise = fetch('head.html').then(response => response.text()).then(data => {
+        const tempContainer = document.createElement('div');
+        tempContainer.innerHTML = data;
+        const templateContent = tempContainer.querySelector('template').content;
+        document.head.appendChild(document.importNode(templateContent, true));
+    });
+
+    const headerPromise = fetch('header.html').then(response => response.text()).then(data => {
+        const tempContainer = document.createElement('div');
+        tempContainer.innerHTML = data;
+        const templateContent = tempContainer.querySelector('template').content;
+        document.body.insertBefore(document.importNode(templateContent, true), document.body.firstChild);
+    });
+
+    const footerPromise = fetch('footer.html').then(response => response.text()).then(data => {
+        const tempContainer = document.createElement('div');
+        tempContainer.innerHTML = data;
+        const templateContent = tempContainer.querySelector('template').content;
+        document.body.appendChild(document.importNode(templateContent, true));
+    });
+
+    // Parallel fetch of head, header, and footer
+    Promise.all([headPromise, headerPromise, footerPromise]).catch(error => {
+        console.error('Error loading templates:', error);
+    });
+});
