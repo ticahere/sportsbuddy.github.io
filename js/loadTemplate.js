5b8e1e90ce6a57464fea4e489468567b25e2e952
Tica Lin <ticalin@Ticas-MacBook-Pro-2.local>
2024-08-19 14:22:04 -0400

    add new article on youtube

--- js/loadTemplate.js
+++ js/loadTemplate.js
@@ -24,4 +24,47 @@ document.addEventListener('DOMContentLoaded', function() {
     Promise.all([headPromise, headerPromise, footerPromise]).catch(error => {
         console.error('Error loading templates:', error);
     });
+    
 });
+
+
+function loadSocialShareButtons(postUrl, postTitle, twitterHandle) {
+    const socialShareElement = document.getElementById('BlogSocialShare');
+    if (!socialShareElement) {
+        console.error('Element with id "BlogSocialShare" not found.');
+        return;
+    }
+    socialShareElement.innerHTML = `
+    Share
+        <a href="https://twitter.com/intent/tweet?text=${encodeURIComponent(postTitle)}&url=${encodeURIComponent(postUrl)}&via=${twitterHandle}" target="_blank"><img src="images/X_logo.jpg"></a>
+        <a href="https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(postUrl)}" target="_blank"><img src="images/LinkedIn.svg"></a>
+        <a href="https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(postUrl)}" target="_blank"><img src="images/Facebook.svg"></a>
+        <a href="https://www.instagram.com/sportsbuddyai/" target="_blank"><img src="images/Instagram.svg"></a>
+    `;
+    
+    const style = document.createElement('style');
+    style.textContent = `
+        .social-share {
+            text-align: end;
+            margin-top: 30px;
+            margin-right:50px;
+        }
+        .social-share a {
+            display: inline-block;
+            padding: 10px;
+            text-decoration: none;
+            border-radius: 5px;
+            color: #333;
+            font-weight: bold;
+        }
+        .social-share a:hover {
+            background-color: #e0e0e0;
+        }
+        .social-share img {
+            width: 1.5rem;
+        }
+    `;
+
+    document.head.appendChild(style);
+    socialShareElement.className = 'social-share';
+}
